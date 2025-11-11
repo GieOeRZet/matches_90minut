5d00e6d507eaeb80c658833e0be4bf9b977b2d50
GieOeRZet <57875682+GieOeRZet@users.noreply.github.com>
2025-11-12 00:23:21 +0100

    Update const.py

--- custom_components/matches_90minut/const.py
+++ custom_components/matches_90minut/const.py
@@ -1,5 +1,8 @@
-"""Constants for 90minut integration."""
+"""Stałe dla integracji Matches 90minut."""
 
 DOMAIN = "matches_90minut"
-DEFAULT_LAST = 3
-DEFAULT_NEXT = 3
+
+DEFAULT_LAST_MATCHES = 5
+DEFAULT_NEXT_MATCHES = 1
+DEFAULT_TEAM_ID = "101"
+DEFAULT_SEASON_ID = ""
