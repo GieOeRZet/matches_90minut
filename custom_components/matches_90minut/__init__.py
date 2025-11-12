024f92429231f6ab9a8fd3e9313112fffb1fbec4
GieOeRZet <57875682+GieOeRZet@users.noreply.github.com>
2025-11-12 01:05:33 +0100

    Update __init__.py

--- custom_components/matches_90minut/__init__.py
+++ custom_components/matches_90minut/__init__.py
@@ -1,21 +1,24 @@
-"""Init for Matches 90minut integration."""
-import logging
-from homeassistant.core import HomeAssistant
+"""Initialize the 90minut.pl integration."""
+from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 
-DOMAIN = "matches_90minut"
-PLATFORMS = ["sensor"]
-
-_LOGGER = logging.getLogger(__name__)
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up Matches 90minut from a config entry."""
-    _LOGGER.info("✅ Integracja %s uruchomiona pomyślnie.", DOMAIN)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up integration from config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload integration entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload integration when options are updated."""
+    await hass.config_entries.async_reload(entry.entry_id)
